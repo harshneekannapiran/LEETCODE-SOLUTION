aabb26362a21418d44a5f8d25319de80b61e3628
HARSHNEE <155135299+harshneekannapiran@users.noreply.github.com>
2026-02-08 19:43:51 +0530

    Time: 111 ms (10.26%) | Memory: 8.9 MB (21.03%) - LeetSync

---
+++ 1-two-sum/two-sum.c
@@ -0,0 +1,13 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    static int a[2];
+    for(int i=0;i<numsSize;i++){
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[i]+nums[j]==target){
+                a[0]=i;
+                a[1]=j;
+                *returnSize=2;           
+            }
+        }
+    }
+      return a;
+}
