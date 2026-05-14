31a31d31dfefa5e494f89022ba6c0ca1effec018
HARSHNEE <155135299+harshneekannapiran@users.noreply.github.com>
2026-05-14 22:11:43 +0530

    Time: 16 ms (21.74%) | Memory: 46.7 MB (8.83%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap <Character,Integer> map1=new HashMap();
+        HashMap <Character,Integer> map2=new HashMap();
+
+        for(char ch:s.toCharArray()){
+            if(map1.containsKey(ch)){
+                map1.put(ch,map1.get(ch)+1);
+            }
+            else{
+                map1.put(ch,1);
+            }
+        }
+
+        for(char ch:t.toCharArray()){
+            if(map2.containsKey(ch)){
+                map2.put(ch,map2.get(ch)+1);
+            }
+            else{
+                map2.put(ch,1);
+            }
+        }
+        return map1.equals(map2);
+    }
+}
