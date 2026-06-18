fc9b7036272945dff3598c26336f59d48e6e0e87
HARSHNEE <155135299+harshneekannapiran@users.noreply.github.com>
2026-06-18 22:54:42 +0530

    Time: 3 ms (95.32%) | Memory: 45.2 MB (22.16%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,31 +1,33 @@
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer>map1=new HashMap();
-         HashMap<Integer,Integer>map2=new HashMap();
 
-         for(int num1:nums1){
-            map1.put(num1,map1.getOrDefault(num1,0)+1);
-         }
+        HashMap<Integer,Integer> map = new HashMap<>();
 
-         for(int num2:nums2){
-            map2.put(num2,map2.getOrDefault(num2,0)+1);
-         }
-         
-         ArrayList <Integer> list=new ArrayList<>();
-        
-          for(int key:map1.keySet()){
-            if(map2.containsKey(key)){
-                int times=Math.min(map1.get(key),map2.get(key));
-                while(times>0){
-                    list.add(key);
-                    times--;
-                }
+        // Store frequency of nums1
+        for(int num : nums1){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Traverse nums2
+        for(int num : nums2){
+
+            if(map.containsKey(num) && map.get(num) > 0){
+
+                list.add(num);
+
+                map.put(num, map.get(num)-1);
             }
-          }
-          int res[]=new int[list.size()];
-          for(int i=0;i<list.size();i++){
+        }
+
+        int[] res = new int[list.size()];
+
+        for(int i=0;i<list.size();i++){
             res[i]=list.get(i);
-          }
-          return res;
+        }
+
+        return res;
     }
 }
