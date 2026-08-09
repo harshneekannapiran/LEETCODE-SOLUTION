fc74087d322822dda910195fe5fe04dd4d6a8630
HARSHNEE <155135299+harshneekannapiran@users.noreply.github.com>
2026-08-09 10:17:39 +0530

    Time: 16 ms (21.09%) | Memory: 52.6 MB (80.69%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -6,11 +6,9 @@ class Solution {
 
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
-        }
 
-        for(int key:map.keySet()){
-            if(map.get(key)>n/2)
-                return key;
+            if(map.get(num)>n/2)
+                return num;
         }
         return -1;
     }
