f220abfe1ee4305e809fe29834c2dade6b8932c4
HARSHNEE <155135299+harshneekannapiran@users.noreply.github.com>
2026-08-11 21:35:24 +0530

    Time: 3 ms (57.92%) | Memory: 44.2 MB (74.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPalindrome(String s) {
-         s=s.toLowerCase();
-         int left=0;
-         int right=s.length()-1;
-         while(left<right){
+        s=s.toLowerCase();
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
             while(left<right && (!Character.isLetterOrDigit(s.charAt(left)))){
                 left++;
             }
@@ -11,11 +11,11 @@ class Solution {
                 right--;
             }
             if(s.charAt(left)!=s.charAt(right)){
-                  return false;
-         }
+                return false;
+            }
             left++;
-            right--  ; 
+            right--;
+        }
+        return true;
     }
-    return true;
-}
 }
