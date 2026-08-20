1a7578c8bd24dacc88b48715efe5b56c0a96538a
HARSHNEE <155135299+harshneekannapiran@users.noreply.github.com>
2026-08-20 19:13:51 +0530

    Time: 1469 ms (5.02%) | Memory: 47.7 MB (15.09%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,32 +1,19 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int pfreq []=new int[26];
-        int sfreq []=new int[26];
-        ArrayList <Integer> list=new ArrayList<>();
-
-        if(p.length()>s.length()){
+        ArrayList<Integer> list= new ArrayList<>();
+        if(p.length()>s.length())
             return list;
-        }
-        for(int i=0;i<p.length();i++){
-            char ch=p.charAt(i);
-            pfreq[ch-'a']++;
-        }
-
-         for(int i=0;i<p.length();i++){
-            char ch=s.charAt(i);
-            sfreq[ch-'a']++;
-        }
-        if(Arrays.equals(sfreq,pfreq))
-            list.add(0);
         int n=p.length();
-        for(int i=p.length();i<s.length();i++){
-            char left=s.charAt(i-n);
-            char right=s.charAt(i);
-            sfreq[left-'a']--;
-            sfreq[right-'a']++;
+        char c1[]=p.toCharArray();
+        Arrays.sort(c1);
 
-            if(Arrays.equals(sfreq,pfreq))
-                    list.add(i-n+1);
+        for(int i=0;i<=s.length()-n;i++){
+            String sub=s.substring(i,i+n);
+            char c2[]=sub.toCharArray();
+            Arrays.sort(c2);
+
+        if(Arrays.equals(c1,c2))
+            list.add(i);
         }
         return list;
     }
